a1e588e4d24db043509acc8367b42e9b5983eb4b
GubarevaAlena <100828174+GubarevaAlena@users.noreply.github.com>
2022-10-10 01:19:19 +0300

    Create Hero.cs
    Был сделан для анимация, но анимации отказались работать, так что он бесполезен, надо будет убрать

---
+++ Hero.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treasuremaze
+{
+    public static class Hero
+    {
+        public static int IdleFrame = 1;
+        public static int UpFrame = 3;
+        public static int DownFrame = 3;
+        public static int RightFrame = 3;
+        public static int LeftFrame = 3;
+    }
+}
