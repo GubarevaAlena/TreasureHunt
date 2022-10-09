547b54501574682907036c81890b90b69c9c56b3
GubarevaAlena <100828174+GubarevaAlena@users.noreply.github.com>
2022-10-10 01:24:14 +0300

    Create Chest.cs
    Пока что пустой класс для спона сундуков

---
+++ Chest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treasuremaze
+{
+    public class Chest
+    {
+        public int PosX;
+        public int PosY;
+    }
+}
