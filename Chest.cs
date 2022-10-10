ea77e5b7653c8d61dc4a99f2308fa5ffc69c6b39
GubarevaAlena <100828174+GubarevaAlena@users.noreply.github.com>
2022-10-10 18:39:21 +0300

    Update Chest.cs

--- Chest.cs
+++ Chest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,20 @@ namespace treasuremaze
     {
         public int PosX;
         public int PosY;
+        public int size;
+        public Image SpriteSheet;
+
+        public Chest(int posX, int posY, Image spriteSheet)
+        {
+            this.PosX = posX;
+            this.PosY = posY;
+            size = 17;
+            this.SpriteSheet = spriteSheet;
+        }
+        public void AddChest(Graphics g)
+        { 
+            g.DrawImage(SpriteSheet, new Rectangle(new Point(PosX, PosY), new Size(size, size)), 1, 1, size, size, GraphicsUnit.Pixel);
+        }
+
     }
 }
